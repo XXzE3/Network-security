f5e17394d0bf6d24df1f740cc48712abf44d073b
Pritam <pritamcon02@gmail.com>
2024-08-22 05:12:40 +0530

    update get_data to convert and upload

--- get_data.py
+++ get_data.py
@@ -5,7 +5,7 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 MONGO_DB_URL=os.getenv("MONGO_DB_URL")
-print(MONGO_DB_URL)
+# print(MONGO_DB_URL)
 
 
 import certifi
@@ -16,3 +16,35 @@ import numpy as np
 import pymongo
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def csv_to_json_converter(self,files_path):
+        try:
+            data = pf.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def pushing_data_to_mongoDB(self,records,database,collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.databse = self.mongo_client(self.database)
+            self.collection = self.mongo_client(self.collection)
+            self.collection.insert_many(self.records)
+
+            return len(self.records)
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
