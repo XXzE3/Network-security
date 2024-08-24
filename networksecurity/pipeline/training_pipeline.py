b8231fecd7abdf16c8ec12779daaae1e690595a3
Pritam <pritamcon02@gmail.com>
2024-08-24 22:17:16 +0530

    Initiated the classes and coded the run_pipeline for test data ingestion run

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -6,10 +6,10 @@ from networksecurity.logger.logger import logging
 
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.components.data_validation import DataTransformation
-from networksecurity.components.data_validation import ModelTrainer
-from networksecurity.components.data_validation import ModelEvaluation
-from networksecurity.components.data_validation import ModelPusher
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.components.model_evaluation import ModelEvaluation
+from networksecurity.components.model_pusher import ModelPusher
 
 from networksecurity.entity.config_entity import (
     DataIngestionConfig,
@@ -76,6 +76,7 @@ class TrainingPipeline:
     
     def run_pipeline(self):
         try:
-            pass
+            data_ingestion_artifact = self.start_data_ingestion()
+            print(data_ingestion_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
