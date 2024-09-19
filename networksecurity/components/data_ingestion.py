9fbe948ba966f120fad152226b542e9ba9305f47
Pritam <pritamcon02@gmail.com>
2024-09-19 17:24:09 +0530

    MongoDB message changed

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -30,8 +30,8 @@ class DataIngestion:
             collection_name = self.data_ingestion_config.collection_name
 
             self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
-            print('MongoDB connected')
             collection = self.mongo_client[database_name][collection_name]
+            print('MongoDB connected')
             
             df = pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
