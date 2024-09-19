f4a3046db1f5bcc72edcc8091d76f35f65806e00
Pritam <pritamcon02@gmail.com>
2024-09-19 22:09:08 +0530

    model trainer initiated

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -62,9 +62,12 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_model_trainer(self):
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact):
         try:
-            pass
+            model_trainer_config = ModelTrainerConfig(self.training_pipeline_config)
+            model_trainer = ModelTrainer(data_transformation_artifact = data_transformation_artifact, model_trainer_config = model_trainer_config)
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -88,5 +91,7 @@ class TrainingPipeline:
             print(data_validation_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             print(data_transformation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            print(model_trainer_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
